ff5529717ef9ccb59fa93e4caccd5a3d9d12dd92
Jaydip Tank <95556824+Jaydip04@users.noreply.github.com>
2024-06-26 11:21:39 +0530

    list_widget file

---
+++ lib/List_Widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class list_widget extends StatefulWidget {
+  const list_widget({super.key});
+
+  @override
+  State<list_widget> createState() => _list_widgetState();
+}
+
+class _list_widgetState extends State<list_widget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RefreshIndicator(
+          // triggerMode: RefreshIndicatorTriggerMode.anywhere,
+          triggerMode: RefreshIndicatorTriggerMode.onEdge,
+          edgeOffset: 50,
+          backgroundColor: Colors.black,
+          displacement: 100,
+          strokeWidth: 3,
+          color: Colors.white,
+          onRefresh: _refresh,
+            child: ListView()),
+      ),
+    );
+  }
+
+  Future<void> _refresh() async {
+    return Future.delayed(Duration(seconds: 5));
+  }
+}
